f6a47a81d7dc1b41b09114ed6012a208f87d6334
Timo654 <41972888+Timo654@users.noreply.github.com>
2025-03-29 12:36:45 +0200

    gameplay loop base

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static event Action<bool> GameActive;
+    public static event Action<int> OnGameTimeChanged;
+    public static event Action<float> BoostSpeed;
+    private bool gameActive = false;
+    private float currentTime = 60f;
+    private int lastTimeValue;
+    private int currentPhase = 0;
     private void OnEnable()
     {
         LevelChanger.OnFadeInFinished += StartGame;
+        GameActive += SetActive;
     }
     private void OnDisable()
     {
         LevelChanger.OnFadeInFinished -= StartGame;
+        GameActive -= SetActive;
+    }
+
+    private void SetActive(bool val)
+    {
+        gameActive = val;
     }
 
     private void Awake()
@@ -21,10 +34,45 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         LevelChanger.Instance.FadeIn();
+        // PLAY GAMEPLAY MUSIC HERE
     }
     private void StartGame()
     {
         GameActive.Invoke(true);
         Time.timeScale = 1.0f;
+    }
+
+    private void Update()
+    {
+        if (!gameActive) return;
+        if (currentTime <= 0)
+        {
+            //EndGame();
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
+
+        if (lastTimeValue != (int)currentTime)
+        {
+            lastTimeValue = (int)currentTime;
+            OnGameTimeChanged?.Invoke(lastTimeValue);
+        }
+
+        if (currentPhase == 0 && currentTime < 30f)
+        {
+            //BoostSpeed?.Invoke(5f);
+            currentPhase++; // enter phase 2, which is faster
+            // SWITCH TO PHASE 2 THEME HERE
+        }
+    }
+
+    private void EndGame()
+    {
+        // TODO - handle all the different endings etc etc
+        GameActive?.Invoke(false);
+        Debug.Log("TODO - game end");
+        LevelChanger.Instance.FadeToLevel("MainMenu");
     }
 }
