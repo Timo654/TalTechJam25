80b804cc517bf2e0f61c0d09c332e793a0114ce6
Timo654 <41972888+Timo654@users.noreply.github.com>
2025-03-29 23:53:07 +0200

    initial timer implementation

--- Assets/Scripts/UI/HUDUpdater.cs
+++ Assets/Scripts/UI/HUDUpdater.cs
@@ -1,16 +1,40 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class HUDUpdater : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private TextMeshProUGUI m_scoreText;
+    private TextMeshProUGUI m_timerText;
+
+    private void Awake()
     {
-        
+        m_scoreText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        m_timerText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+    }
+    private void OnEnable()
+    {
+        ChaosCounter.UpdateScore += UpdateScoreUI;
+        GameManager.OnGameTimeChanged += UpdateTimerUI;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        ChaosCounter.UpdateScore -= UpdateScoreUI;
+        GameManager.OnGameTimeChanged -= UpdateTimerUI;
+    }
+
+    // fancier - lerp shit
+    private void UpdateTimerUI(int time)
+    {
+        int minutes = time / 60;
+        int seconds = time % 60;
+        m_timerText.text = $"Time: {minutes:D2}:{seconds:D2}";
+    }
+
+    private void UpdateScoreUI(int score)
+    {
+        m_scoreText.text = $"Score: {score}";
     }
 }
+
